a5d7430d4b40ab496a2c197542e6fcf5c3d45dc9
Anshu Mittal <anshumittal200421@gmail.com>
2025-04-17 00:08:48 +0530

    all setup configerd

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.use(cors({
     credentials: true,
 }))
 
+// routes
+import userRouter from  './routes/user.routes.js';
+
+// routes declaration 
+app.use("/api/v1/users", userRouter);
+
+// https:localhost:8000/api/v1/users/register
+
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
